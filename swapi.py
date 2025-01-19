f2bcd5c9d96d52fa239052dd0c7ec02cd18f0059
Егор Осипов <egor.osipov.a@yandex.ru>
2025-01-19 21:22:04 +0300

    ready

--- swapi.py
+++ swapi.py
@@ -1,13 +1,16 @@
-import logging
-import requests
-from typing import Optional
+from pathlib import Path
 from urllib.parse import urlparse, ParseResult
+import requests
+from typing import KeysView
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+def save_sw_data():
+    sw_requester = SWRequester('https://swapi.dev/api')
+    Path("data").mkdir(exist_ok=True)
+    categories = sw_requester.get_sw_categories()
+    for category in categories:
+        with open(f'data/{category}.txt', 'w') as file:
+            file.write(str(sw_requester.get_sw_info(category)))
 
 
 class APIRequester:
@@ -16,86 +19,56 @@ class APIRequester:
         if not parsed.scheme or not parsed.netloc:
             raise ValueError(f'Не корректный URL: {base_url}')
 
-        self.base_url = base_url
+        self.base_url: str = base_url
 
     def get(
         self,
         endpoint: str = ''
-    ) -> Optional[requests.Response]:
+    ) -> requests.Response:
         try:
             full_url: str = (
                 self.base_url.rstrip('/')
                 + '/' + endpoint.lstrip('/')
             )
-            response: requests.Response = requests.get(full_url)
+            response: requests.Response = requests.get(full_url, timeout=10)
             response.raise_for_status()
-
-        except requests.HTTPError as e:
-            logging.error(f'<Ошибка на сервере: {e}>')
-            return None
-
-        except requests.ConnectionError as e:
-            logging.error(f'<Cетевая ошибка: {e}>')
-            return None
-
-        except requests.ConnectTimeout as e:
-            logging.error(
-                f'<Время ожидания запроса истекло при подключении: {e}>'
-                )
-            return None
-
-        except requests.Timeout as e:
-            logging.info(f'<Время ожидания запроса истекло: {e}>')
-            return None
-
-        except Exception as e:
-            logging.info(f'<Ошибка в ответе: {e}>')
-            return None
-
-        else:
-            logging.info('<Получен успешный ответ>')
             return response
+        except requests.TooManyRedirects:
+            print('Слишком много перенаправлений.')
+        except requests.URLRequired:
+            print('Для отправки запроса требуется действительный URL')
+        except requests.HTTPError:
+            print('Ошибка на сервере')
+        except requests.ConnectionError:
+            print('Cетевая ошибка')
+        except requests.Timeout:
+            print(
+                'Время ожидания запроса истекло при подключении'
+                )
+        except requests.Timeout:
+            print('Время ожидания запроса истекло')
+        except requests.RequestException:
+            print(
+                'Возникла ошибка при выполнении запроса'
+                )
+        except Exception:
+            print('Ошибка в ответе')
 
 
 class SWRequester(APIRequester):
-    def __init__(self):
-        super().__init__('https://swapi.dev/api/')
-
-    def get_sw_categories(self) -> list:
-        response: Optional[requests.Response] = self.get()
-        if response is None:
-            logging.info('<Запрос к API не удался>')
-            return []
+    def get_sw_categories(self) -> KeysView[str]:
+        response: requests.Response = self.get()
         try:
-            response_json = response.json()
+            response_json: dict = response.json()
+        except requests.JSONDecodeError:
+            print('<Не получилось закодировать в JSON>')
+        except Exception:
+            print('Не понятная ошибка')
+        return response_json.keys()
 
-        except ValueError:
-            logging.info('<Не получилось закодировать в JSON>')
-            return []
-
-        except Exception as e:
-            logging.info(f'Ошибка: {e}')
-            return []
-
-        return list(response_json.keys())
-
-    def get_sw_info(self, sw_type: str) -> Optional[requests.Response]:
-        available_categories: list = self.get_sw_categories()
-        if sw_type in available_categories:
-            endpoint: str = sw_type.lstrip('/')
-            response: Optional[requests.Response] = self.get(
-                endpoint.rstrip('/') + '/'
-            )
-            if response is None:
-                logging.error('Ошибка в запросе')
-            return response
-        else:
-            logging.error(
-                f'<Этой категории - {sw_type} нет '
-                'в доступных: {available_categories}>'
+    def get_sw_info(self, sw_type: str) -> str:
+        endpoint: str = sw_type.lstrip('/')
+        response: requests.Response = self.get(
+                    endpoint.rstrip('/') + '/'
                 )
-            return None
-
-
-api_swapi = SWRequester()
-print(api_swapi.get_sw_categories())
+        return response.text
